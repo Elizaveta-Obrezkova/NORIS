8459c563ec1bb75113b5838e7f0652ec833dbb8e
Elizaveta <liza.sidorchuk@mail.ru>
2025-04-11 17:07:42 +0300

    feat: add yopokki

--- index.js
+++ index.js
@@ -6,13 +6,17 @@ const openMenuButton = document.querySelector('.header__button-nav');
 const popup = document.querySelector('.popup');
 const cardsChips = document.querySelectorAll('.product-card_chips');
 const cardsPasta = document.querySelectorAll('.product-card_pasta');
+const cardsYopokki = document.querySelectorAll('.product-card_yopokki');
 const previousButtonChips = document.querySelector('.products__slider-chips-button_previous');
 const nextButtonChips = document.querySelector('.products__slider-chips-button_next');
 const previousButtonPasta = document.querySelector('.products__slider-pasta-button_previous');
 const nextButtonPasta = document.querySelector('.products__slider-pasta-button_next');
+const previousButtonYopokki = document.querySelector('.products__slider-yopokki-button_previous');
+const nextButtonYopokki = document.querySelector('.products__slider-yopokki-button_next');
 
 let cardChipsIndex = 0;
 let cardPastaIndex = 0;
+let cardYopokkiIndex = 0;
 
 
 openMenuButton.addEventListener('click', () => {
@@ -37,6 +41,12 @@ function showPasta(index) {
   cardsPasta[cardPastaIndex].classList.add('product-card_hidden');
   cardsPasta[index].classList.remove('product-card_hidden');
   cardPastaIndex = index;
+};
+
+function showYopokki(index) {
+  cardsYopokki[cardYopokkiIndex].classList.add('product-card_hidden');
+  cardsYopokki[index].classList.remove('product-card_hidden');
+  cardYopokkiIndex = index;
 };
 
 previousButtonChips.addEventListener('click', () => {
@@ -75,5 +85,24 @@ nextButtonPasta.addEventListener('click', () => {
   showPasta(index)
 });
 
+previousButtonYopokki.addEventListener('click', () => {
+  let index = cardYopokkiIndex - 1;
+  if (index < 0) {
+    index = cardsYopokki.length - 1
+  }
+
+  showYopokki(index)
+});
+
+nextButtonYopokki.addEventListener('click', () => {
+  let index = cardYopokkiIndex + 1;
+  if (index >= cardsYopokki.length) {
+    index = 0;
+  }
+
+  showYopokki(index)
+});
+
 showChips(cardChipsIndex);
 showPasta(cardPastaIndex);
+showYopokki(cardYopokkiIndex);
